c795ca391ca4ebe7ef3de809c480e456241ab91e
LuisAnzules <abluis.correo@gmail.com>
2025-01-16 09:00:39 +0100

    Refactorizacion Apartado 1

--- apartado1/src/main/control/Main.java
+++ apartado1/src/main/control/Main.java
@@ -1,12 +1,5 @@
 package main.control;
 
-import jakarta.persistence.NoResultException;
-import main.model.Autor;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
